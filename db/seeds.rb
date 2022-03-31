eaa8bf8a96633a14f50a9de2353a5213a8bb9192
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-31 13:32:18 +0200

    Create database with greetings table and a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greet_one = Greeting.create(greeting: 'Hello there, welcome to my rails-react app')
+greet_two = Greeting.create(greeting: 'Ola!, welcome to my rails-react app')
+greet_three = Greeting.create(greeting: "I hope you're doing well, welcome to my rails-react app")
+greet_four = Greeting.create(greeting: "I hope you're having a great week, welcome to my rails-react app")
+greet_five = Greeting.create(greeting: 'Hi everyone, welcome to my rails-react app')
